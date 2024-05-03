d67dc6bc9697c044b0467df2d7f393591ea02b1b
faruq <yeemirahman@gmail.com>
2024-05-03 15:28:06 +0100

    completed

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,18 @@
 // TODO: Write your function in this file
-
+function fizzBuzzRange(lower, upper) {
+    let result = [];
+    for (let i = lower; i <= upper; i++) {
+        if (i % 15 === 0) {
+            result.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            result.push("Fizz");
+        } else if (i % 5 === 0) {
+            result.push("Buzz");
+        } else {
+            result.push(i);
+        }
+    }
+    return result;
+}
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzBuzzRange;
